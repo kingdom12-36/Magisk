dee491092431ed7fd57d312f2e7a5371ffb90c2b
00 <shamsaver1@gmail.com>
2026-07-27 06:45:46 +0300

    rename: Magisk → ShadowMask (paths, SELinux, package)

--- scripts/module_installer.sh
+++ scripts/module_installer.sh
@@ -9,9 +9,9 @@ umask 022
 # echo before loading util_functions
 ui_print() { echo "$1"; }
 
-require_new_magisk() {
+require_new_shadowmask() {
   ui_print "*******************************"
-  ui_print " Please install Magisk v20.4+! "
+  ui_print " Please install ShadowMask v20.4+! "
   ui_print "*******************************"
   exit 1
 }
@@ -25,9 +25,9 @@ ZIPFILE=$3
 
 mount /data 2>/dev/null
 
-[ -f /data/adb/magisk/util_functions.sh ] || require_new_magisk
-. /data/adb/magisk/util_functions.sh
-[ $MAGISK_VER_CODE -lt 20400 ] && require_new_magisk
+[ -f /data/adb/shadowmask/util_functions.sh ] || require_new_shadowmask
+. /data/adb/shadowmask/util_functions.sh
+[ $MAGISK_VER_CODE -lt 20400 ] && require_new_shadowmask
 
 install_module
 exit 0
