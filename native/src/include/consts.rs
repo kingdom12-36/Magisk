b946c5a85f05d90b9d502e5d8dd6893e616bab3a
00 <shamsaver1@gmail.com>
2026-07-27 06:45:26 +0300

    rename: Magisk → ShadowMask (paths, SELinux, package)

--- native/src/include/consts.rs
+++ native/src/include/consts.rs
@@ -9,21 +9,21 @@ pub const APPLET_NAMES: &[&str] = &["su", "resetprop"];
 
 // versions
 pub use flags::*;
-pub const MAGISK_FULL_VER: &str = concatcp!(MAGISK_VERSION, "(", MAGISK_VER_CODE, ")");
+pub const SHADOWMASK_FULL_VER: &str = concatcp!(MAGISK_VERSION, "(", MAGISK_VER_CODE, ")");
 
 pub const APP_PACKAGE_NAME: &str = "com.shadowmask";
 
-pub const LOGFILE: &str = "/cache/magisk.log";
+pub const LOGFILE: &str = "/cache/shadowmask.log";
 
 // data paths
 pub const SECURE_DIR: &str = "/data/adb";
 pub const MODULEROOT: &str = concatcp!(SECURE_DIR, "/modules");
 pub const MODULEUPGRADE: &str = concatcp!(SECURE_DIR, "/modules_update");
 pub const DATABIN: &str = concatcp!(SECURE_DIR, "/magisk");
-pub const MAGISKDB: &str = concatcp!(SECURE_DIR, "/magisk.db");
+pub const SHADOWMASKDB: &str = concatcp!(SECURE_DIR, "/shadowmask.db");
 
 // tmpfs paths
-pub const INTERNAL_DIR: &str = ".magisk";
+pub const INTERNAL_DIR: &str = ".shadowmask";
 pub const MAIN_CONFIG: &str = concatcp!(INTERNAL_DIR, "/config");
 pub const PREINITMIRR: &str = concatcp!(INTERNAL_DIR, "/preinit");
 pub const MODULEMNT: &str = concatcp!(INTERNAL_DIR, "/modules");
@@ -38,11 +38,11 @@ pub const ROOTMNT: &str = concatcp!(ROOTOVL, "/.mount_list");
 pub const SELINUXMOCK: &str = concatcp!(INTERNAL_DIR, "/selinux");
 
 // Unconstrained domain the daemon and root processes run in
-pub const SEPOL_PROC_DOMAIN: &str = "magisk";
-pub const MAGISK_PROC_CON: &str = concatcp!("u:r:", SEPOL_PROC_DOMAIN, ":s0");
+pub const SEPOL_PROC_DOMAIN: &str = "shadowmask";
+pub const SHADOWMASK_PROC_CON: &str = concatcp!("u:r:", SEPOL_PROC_DOMAIN, ":s0");
 // Unconstrained file type that anyone can access
-pub const SEPOL_FILE_TYPE: &str = "magisk_file";
-pub const MAGISK_FILE_CON: &str = concatcp!("u:object_r:", SEPOL_FILE_TYPE, ":s0");
+pub const SEPOL_FILE_TYPE: &str = "shadowmask_file";
+pub const SHADOWMASK_FILE_CON: &str = concatcp!("u:object_r:", SEPOL_FILE_TYPE, ":s0");
 // Log pipe that only root and zygote can open
-pub const SEPOL_LOG_TYPE: &str = "magisk_log_file";
-pub const MAGISK_LOG_CON: &str = concatcp!("u:object_r:", SEPOL_LOG_TYPE, ":s0");
+pub const SEPOL_LOG_TYPE: &str = "shadowmask_log_file";
+pub const SHADOWMASK_LOG_CON: &str = concatcp!("u:object_r:", SEPOL_LOG_TYPE, ":s0");
